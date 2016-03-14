d41b66943b6e75633742b740fd3a916582564885
maggie1994 <tuonglinhtong@gmail.com>
2016-03-14 22:49:33 +0700

    my acount page

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -14,4 +14,17 @@ $(document).ready(function() {
       document.getElementById('my_file').click();
     };
   } catch(err){console.clear();}
+
+  //USER PROFILE
+  var current = $('.list-info > li:first-child');
+  var current_profile = 1;
+  var numProfile = $('.list-info > li').size();
+  $('.list-info > li').click(function(){
+    current.removeClass('active');
+    $('.profile-info > .info-content:nth-of-type(' + current_profile + ')').css('display','none');
+    current_profile = $(this).index() + 1;
+    current = $(this);
+    current.addClass('active');
+    $('.profile-info > .info-content:nth-of-type(' + current_profile + ')').css('display','block');
+  });
 });
